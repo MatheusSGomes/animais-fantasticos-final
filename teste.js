38e913f72e2c39572bd4f07590fafc6be6e9d97e
Matheus Gomes <math.s.gomes@gmail.com>
2021-05-16 11:49:20 -0300

    atualizações

--- teste.js
+++
@@ -1,9 +0,0 @@
-// const path = require('path');
-import * as path from 'path';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-console.log(path.resolve(__dirname, 'a.js'))
